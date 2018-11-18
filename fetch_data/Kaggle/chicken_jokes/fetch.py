81c310b24795b6e2940292ca4725fbedb1098f4b
James1729 <james.pitkin@deliveroo.co.uk>
2018-11-18 11:56:12 +0000

    created fetch_data directory

---
+++ fetch_data/Kaggle/chicken_jokes/fetch.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext_format_version: '1.3'
+#   jupytext_formats: py:light
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.6
+# ---
+
+# # fetch Kaggle short joke data and extract chicken jokes
+
+import pandas as pd
+
+rawjokes = pd.read_csv("../../../Data/Kaggle/shortjokes.csv", index_col=0) 
+rawjokes.head(10)
+
+
